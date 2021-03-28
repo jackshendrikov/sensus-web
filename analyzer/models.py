bbb194681b6be8b57249276d377e90c7e152b323
Jack Shendrikov <jackshendrikov@gmail.com>
2021-03-28 18:57:59 +0300

    Added data processing and prediction methods

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from re import match
+from nltk.tokenize import RegexpTokenizer
+from tensorflow.keras.models import load_model
 
-# Create your models here.
+from sentimento.settings import WordVec, graph
+
+import pymorphy2
+import numpy as np
+
+
+class Sentence(models.Model):
+
+    def __init__(self, sent, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sent = sent
+
+    STOP_WORDS = set(line.strip() for line in open('../model/stopwords_ua.txt', mode="r", encoding="utf-8"))
+    MORPH = pymorphy2.MorphAnalyzer(lang='uk')
+    MAX_SEQ_LEN = 100
+
+    @staticmethod
+    def regex_tokenizer(sentence):
+        return RegexpTokenizer(r'\w+').tokenize(sentence)
+
+    @staticmethod
+    def stopwords_elimination(stop_words, sentence):
+        return [w for w in sentence if w not in stop_words]
+
+    @staticmethod
+    def lemmatize_words(morph, words):
+        return [morph.parse(word)[0].normal_form for word in words]
+
+    @staticmethod
+    def irr_words_elimination(words):
+        while True:
+            ready = True
+
+            for word in words:
+                if bool(match(r"[a-zA-Z]", word)) or word.isdigit() or len(word) <= 3:
+                    ready = False
+                    words.remove(word)
+
+            if ready: break
+
+        return words
+
+    def data_preparation(self):
+        words_list = self.regex_tokenizer(self.sent)
+        words_list = self.stopwords_elimination(self.STOP_WORDS, words_list)
+        words_list = self.lemmatize_words(self.MORPH, words_list)
+        words_list = self.irr_words_elimination(' '.join(words_list))
+        return words_list
+
+    @staticmethod
+    def sent_embed(words):
+        sent_embed = []
+        words_list = list(WordVec.vocab)
+
+        for word in words:
+            if word not in words_list:
+                sent_embed.append([0] * 300)
+            elif word in words_list:
+                sent_embed.append(WordVec[word])
+
+        return sent_embed
+
+    @staticmethod
+    def fix_sentence_len(sentence, length):
+        if len(sentence) > length:
+            sentence = sentence[:length]
+        elif len(sentence) < length:
+            for i in range(length - len(sentence)):
+                zeros = [0] * 300
+                sentence.append(zeros)
+        return sentence
+
+    def prediction(self):
+        data = self.data_preparation()
+        data = self.sent_embed(data)
+        data = self.fix_sentence_len(data, self.MAX_SEQ_LEN)
+        data = np.reshape(data, (1, 100, 300))
+
+        with graph.as_default():
+            model = load_model("model/LSTM-CNN-MODEL.h5")  # load model
+            return model.predict(np.array(data))
