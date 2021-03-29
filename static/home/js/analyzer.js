16e0beb064d2829a83b3979fce5fefdf7e68ff1e
Jack Shendrikov <jackshendrikov@gmail.com>
2021-03-30 00:34:02 +0300

    Added header at main template

---
+++ static/home/js/analyzer.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let modeSwitch = document.querySelector('.mode-switch');
+
+  modeSwitch.addEventListener('click', function () {
+    document.documentElement.classList.toggle('dark');
+    modeSwitch.classList.toggle('active');
+  });
+
+});
