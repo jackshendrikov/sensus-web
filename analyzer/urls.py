e537b4f6a4043dc42b038ed53f8606887e70ce16
Jack Shendrikov <jackshendrikov@gmail.com>
2021-03-31 20:34:56 +0300

    Fixed login page

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 from .views import my_view
 
 urlpatterns = [
     path('', my_view, name='my-view'),
-    path('home/', views.home, name='home_page'),
-    path('predict/', views.prediction, name='prediction'),
+    path('home/', login_required(views.home), name='home_page'),
+    path('predict/', login_required(views.prediction), name='prediction'),
 ]
